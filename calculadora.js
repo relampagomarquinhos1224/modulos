e8f459ab5ec25ee76c540bbe483d1d63d629021a
Marcos dos Santos Luiz <marcodossantosluiz111@gmail.com>
2022-04-02 09:36:47 -0300

    1.0.1

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,5 @@
-var somar = require("./modulos/soma")
+var somar = require("./modulosOperacoes/soma");
+var media = require("./modulosOperacoes/media");
 
 console.log(somar(30, 15));
-
+console.log(media(30, 15));
---
+++ modulosOperacoes/media.js
@@ -0,0 +1,5 @@
+var media = function (x, y) {
+    return (x + y) / 2;
+}
+
+module.exports = media;
